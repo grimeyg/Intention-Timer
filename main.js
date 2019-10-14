41044b47f03b4ec33214155b490cd0c316e4a912
Kim McCaskill <kimmccaskill92@gmail.com>
2019-10-14 10:58:04 -0600

    Add minute functionality

--- main.js
+++ main.js
@@ -12,9 +12,11 @@ var errorDescription = document.querySelector(".error-description");
 var toAccomplish = document.querySelector(".to-accomplish");
 // timer
 var secInput = document.getElementById("second-input");
-var min = document.getElementById("minute-input");
+var minInput = document.getElementById("minute-input");
 // timer
 var funcSec = document.getElementById("sec");
+// min edit
+var funcMin = document.getElementById("min");
 
 
 studyButton.addEventListener("click", changeStudy);
@@ -25,13 +27,36 @@ timerStart.addEventListener("click", startCountdown);
 
 
 function startCountdown(){
-var seconds = secInput.value
+var seconds = secInput.value;
 setInterval( countOne, 1000);
 // document.getElementById("timer").innerHTML= count + " secs";
-function countOne(){
- seconds = seconds - 1;
- funcSec.innerHTML = seconds;
-}
+// min edit that doesnt work
+var minutes = minInput.value;
+// setInterval( (countOne, 1000)*60);
+
+
+
+  function countOne(){
+    funcSec.innerHTML = seconds;
+    seconds = seconds - 1;
+    funcMin.innerHTML = minutes;
+    if(seconds === -1){
+      minutes --;
+      seconds = 59;
+      console.log("hi");
+    }
+    if(seconds < 10){
+      seconds = "0" + seconds;
+    }
+    if(minutes === -1){
+      minutes = "00"
+      seconds = "00"
+      return timerStart.innerHTML = "COMPLETE!"
+      //run alert
+    }
+
+    // funcMin.innerHTML = minutes;
+  }
 }
 
 
@@ -49,7 +74,11 @@ function changeForm() {
     toAccomplish.innerText = accomplishInput.value;
     funcSec.innerHTML = secInput.value;
     console.log(funcSec.innerHTML);
-  }
+    funcMin.innerHTML = minInput.value;
+    if(secInput.value < 10){
+      funcSec.innerHTML = "0" + funcSec.innerHTML;
+    }
+}
 }
 
 // function getTime(){
