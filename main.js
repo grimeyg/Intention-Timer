11bafb1d5a8041f1e2f835323e52164a2e9b3d80
“Greg_Grimes” <cggrimes@yahoo.com>
2019-10-14 10:33:22 -0600

    Refactor 3 button functions into 1

--- main.js
+++ main.js
@@ -17,15 +17,44 @@ var min = document.getElementById("minute-input");
 var funcSec = document.getElementById("sec");
 
 
-studyButton.addEventListener("click", changeStudy);
-medButton.addEventListener("click", changeMeditate);
-exeButton.addEventListener("click", changeExercise);
+studyButton.addEventListener("click", function (){
+  changeButton(studyButton, "green")
+});
+medButton.addEventListener("click", function (){
+  changeButton(medButton, "purple")
+});
+exeButton.addEventListener("click", function(){
+  changeButton(exeButton, "orange")
+});
 submitButton.addEventListener("click", changeForm);
 timerStart.addEventListener("click", startCountdown);
 
 
+function changeButton(button, color){
+  // console.log(event, button, color);
+  event.preventDefault();
+  if(button.classList.contains(`change-${color}`)){
+     button.classList.remove(`change-${color}`);
+     timerStart.classList.remove(`${color}-border`);
+} else{
+    button.classList.add(`change-${color}`);
+    timerStart.classList.add(`${color}-border`);
+  }
+  if (color === "green"){
+    medButton.classList.remove("change-purple");
+    exeButton.classList.remove("change-orange");
+  }else if(color === "orange"){
+    studyButton.classList.remove("change-green");
+    medButton.classList.remove("change-purple");
+  }else if(color === "purple"){
+    studyButton.classList.remove("change-green");
+    exeButton.classList.remove("change-orange");
+  }
+}
+
+
 function startCountdown(){
-var seconds = secInput.value
+var seconds = secInput.value;
 setInterval( countOne, 1000);
 // document.getElementById("timer").innerHTML= count + " secs";
 function countOne(){
@@ -52,50 +81,46 @@ function changeForm() {
   }
 }
 
-// function getTime(){
-//   funcSec.innerHTML = secInput.value;
+
+
+
+//
+// function changeStudy(){
+//   event.preventDefault(studyButton);
+//   if(studyButton.classList.contains("change-green")){
+//     studyButton.classList.remove("change-green");
+//     timerStart.classList.remove("green-border");
+// } else{
+//     studyButton.classList.add("change-green");
+//     timerStart.classList.add("green-border");
 // }
-
-
-
-
-
-function changeStudy(){
-  event.preventDefault(studyButton);
-  if(studyButton.classList.contains("change-green")){
-    studyButton.classList.remove("change-green");
-    timerStart.classList.remove("green-border");
-} else{
-    studyButton.classList.add("change-green");
-    timerStart.classList.add("green-border");
-}
-
-medButton.classList.remove("change-purple");
-exeButton.classList.remove("change-orange");
-}
-
-function changeMeditate(){
-  event.preventDefault(medButton);
-  if(medButton.classList.contains("change-purple")){
-    medButton.classList.remove("change-purple");
-    timerStart.classList.remove("purple-border");
-} else{
-    medButton.classList.add("change-purple");
-    timerStart.classList.add("purple-border");
-}
-studyButton.classList.remove("change-green");
-exeButton.classList.remove("change-orange");
-}
-
-function changeExercise(){
-  event.preventDefault(exeButton);
-  if(exeButton.classList.contains("change-orange")){
-    exeButton.classList.remove("change-orange");
-    timerStart.classList.remove("orange-border");
-} else{
-    exeButton.classList.add("change-orange");
-    timerStart.classList.add("orange-border");
-}
-studyButton.classList.remove("change-green");
-medButton.classList.remove("change-purple");
-}
+//
+// medButton.classList.remove("change-purple");
+// exeButton.classList.remove("change-orange");
+// }
+//
+// function changeMeditate(){
+//   event.preventDefault(medButton);
+//   if(medButton.classList.contains("change-purple")){
+//     medButton.classList.remove("change-purple");
+//     timerStart.classList.remove("purple-border");
+// } else{
+//     medButton.classList.add("change-purple");
+//     timerStart.classList.add("purple-border");
+// }
+// studyButton.classList.remove("change-green");
+// exeButton.classList.remove("change-orange");
+// }
+//
+// function changeExercise(){
+//   event.preventDefault(exeButton);
+//   if(exeButton.classList.contains("change-orange")){
+//     exeButton.classList.remove("change-orange");
+//     timerStart.classList.remove("orange-border");
+// } else{
+//     exeButton.classList.add("change-orange");
+//     timerStart.classList.add("orange-border");
+// }
+// studyButton.classList.remove("change-green");
+// medButton.classList.remove("change-purple");
+// }
